6073ed873ad32093d04296313e69ebbf58d0299f
Shiv arora <aroras@cromwell.co.uk>
2016-10-21 10:11:35 +0100

    .gitignore is now working

--- app/src/main/java/com/merlinbusinesssoftware/merlinsignin/ScrollViewListener.java
+++
@@ -1,9 +0,0 @@
-package com.merlinbusinesssoftware.merlinsignin;
-
-/**
- * Created by aroras on 25/05/16.
- */
-public interface ScrollViewListener {
-    void onScrollChanged(ScrollViewExt scrollView,
-                         int x, int y, int oldx, int oldy);
-}
